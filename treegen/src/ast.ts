852a9d11a63fef31b0a830d3572bb4a86beb38e4
Sam Vervaeck <vervaeck.sam@skynet.be>
2020-05-09 22:18:13 +0200

    treegen: Add type definitions for treegen-AST

---
+++ treegen/src/ast.ts
@@ -0,0 +1,49 @@
+
+export interface ReferenceTypeNode {
+  type: 'ReferenceTypeNode';
+  name: string;
+  typeArgs: TypeNode[];
+}
+
+export interface UnionTypeNode {
+  type: 'UnionTypeNode';
+  elements: TypeNode[];
+}
+
+export type TypeNode
+  = ReferenceTypeNode
+  | UnionTypeNode
+
+export interface NodeField {
+  name: string;
+  typeNode: TypeNode;
+}
+
+export interface NodeDeclaration {
+  type: 'NodeDeclaration';
+  name: string;
+  fields: NodeField[];
+}
+
+export interface EnumField {
+  name: string;
+  value: number | null;
+}
+
+export interface EnumDeclaration {
+  type: 'EnumDeclaration';
+  name: string;
+  fields: EnumField[];
+}
+
+export interface TypeDeclaration {
+  type: 'TypeDeclaration';
+  name: string;
+  typeNode: TypeNode;
+}
+
+export type Declaration
+  = NodeDeclaration
+  | TypeDeclaration
+  | EnumDeclaration
+
