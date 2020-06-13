4dcc561a733267a098a647769fe4a75658fc7bed
Sam Vervaeck <vervaeck.sam@skynet.be>
2020-06-13 19:27:39 +0200

    webpack: Add a configuration for production bundle

---
+++ webpack.prod.js
@@ -0,0 +1,30 @@
+
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  target: 'node',
+  mode: 'development',
+  entry: {
+    'bolt': './src/bin/bolt.ts',
+    'bolt-test': './src/bin/bolt-test.ts',
+  },
+  output: {
+    filename: 'bin/[name].js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true }),
+  ],
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: "ts-loader", options: { transpileOnly: true } },
+      { test: /\.m?js$/, exclude: /node_modules/, loader: 'babel-loader' },
+    ]
+  }
+};
+
