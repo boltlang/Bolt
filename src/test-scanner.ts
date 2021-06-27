393fca375e3e272525ae8044dbbe184b043d4475
Sam Vervaeck <samvv@pm.me>
2021-06-28 00:33:20 +0200

    Fix Punctuator tests

--- src/test-scanner.ts
+++ src/test-scanner.ts
@@ -24,21 +24,21 @@ foo.
   b
   c
 `)
-  t.assert(tokens.length === 14);
-  t.assert(tokens[0].type === TokenType.LineFoldStart);
-  t.assert(tokens[1].type === TokenType.Identifier);
-  t.assert(tokens[2].type === TokenType.BlockStart);
-  t.assert(tokens[3].type === TokenType.LineFoldStart);
+  t.assert(tokens.length === 10);
+  //t.assert(tokens[0].type === TokenType.LineFoldStart);
+  t.assert(tokens[0].type === TokenType.Identifier);
+  t.assert(tokens[1].type === TokenType.BlockStart);
+  //t.assert(tokens[3].type === TokenType.LineFoldStart);
+  t.assert(tokens[2].type === TokenType.Identifier);
+  t.assert(tokens[3].type === TokenType.LineFoldEnd);
+  //t.assert(tokens[6].type === TokenType.LineFoldStart);
   t.assert(tokens[4].type === TokenType.Identifier);
   t.assert(tokens[5].type === TokenType.LineFoldEnd);
-  t.assert(tokens[6].type === TokenType.LineFoldStart);
-  t.assert(tokens[7].type === TokenType.Identifier);
-  t.assert(tokens[8].type === TokenType.LineFoldEnd);
-  t.assert(tokens[9].type === TokenType.LineFoldStart);
-  t.assert(tokens[10].type === TokenType.Identifier);
-  t.assert(tokens[11].type === TokenType.LineFoldEnd);
-  t.assert(tokens[12].type === TokenType.BlockEnd);
-  t.assert(tokens[13].type === TokenType.LineFoldEnd);
+  //t.assert(tokens[9].type === TokenType.LineFoldStart);
+  t.assert(tokens[6].type === TokenType.Identifier);
+  t.assert(tokens[7].type === TokenType.LineFoldEnd);
+  t.assert(tokens[8].type === TokenType.BlockEnd);
+  t.assert(tokens[9].type === TokenType.LineFoldEnd);
 });
 
 test('a punctuator correctly punctuates nested blocks', t => {
@@ -49,24 +49,25 @@ a.
     d
     e
 `)
-  t.assert(tokens.length === 19);
-  t.assert(tokens[0].type === TokenType.LineFoldStart);
-  t.assert(tokens[1].type === TokenType.Identifier);
-  t.assert(tokens[2].type === TokenType.BlockStart);
-  t.assert(tokens[3].type === TokenType.LineFoldStart);
+  t.assert(tokens.length === 14);
+  //t.assert(tokens[0].type === TokenType.LineFoldStart);
+  t.assert(tokens[0].type === TokenType.Identifier);
+  t.assert(tokens[1].type === TokenType.BlockStart);
+  //t.assert(tokens[3].type === TokenType.LineFoldStart);
+  t.assert(tokens[2].type === TokenType.Identifier);
+  t.assert(tokens[3].type === TokenType.BlockStart);
+  //t.assert(tokens[6].type === TokenType.LineFoldStart);
   t.assert(tokens[4].type === TokenType.Identifier);
-  t.assert(tokens[5].type === TokenType.BlockStart);
-  t.assert(tokens[6].type === TokenType.LineFoldStart);
-  t.assert(tokens[7].type === TokenType.Identifier);
-  t.assert(tokens[8].type === TokenType.LineFoldEnd);
-  t.assert(tokens[9].type === TokenType.LineFoldStart);
-  t.assert(tokens[10].type === TokenType.Identifier);
+  t.assert(tokens[5].type === TokenType.LineFoldEnd);
+  //t.assert(tokens[9].type === TokenType.LineFoldStart);
+  t.assert(tokens[6].type === TokenType.Identifier);
+  t.assert(tokens[7].type === TokenType.LineFoldEnd);
+  //t.assert(tokens[12].type === TokenType.LineFoldStart);
+  t.assert(tokens[8].type === TokenType.Identifier);
+  t.assert(tokens[9].type === TokenType.LineFoldEnd);
+  t.assert(tokens[10].type === TokenType.BlockEnd);
   t.assert(tokens[11].type === TokenType.LineFoldEnd);
-  t.assert(tokens[12].type === TokenType.LineFoldStart);
-  t.assert(tokens[13].type === TokenType.Identifier);
-  t.assert(tokens[14].type === TokenType.LineFoldEnd);
-  t.assert(tokens[15].type === TokenType.BlockEnd);
-  t.assert(tokens[17].type === TokenType.BlockEnd);
-  t.assert(tokens[18].type === TokenType.LineFoldEnd);
-});
+  t.assert(tokens[12].type === TokenType.BlockEnd);
+  t.assert(tokens[13].type === TokenType.LineFoldEnd);
+})
 
