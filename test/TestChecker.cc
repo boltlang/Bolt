15dab8a7a80dc9f772736b2e70079bc68a6eac2d
Sam Vervaeck <samvv@pm.me>
2024-07-10 12:04:03 +0200

    Fix compile errors in tests

--- test/TestChecker.cc
+++ test/TestChecker.cc
@@ -14,14 +14,14 @@ auto checkSourceFile(std::string Input) {
   DiagnosticStore DS;
   TextFile T { "#<anonymous>", Input };
   VectorStream<std::string, Char> Chars { Input, EOF };
-  Scanner S(T, Chars);
+  Scanner S { DS, T, Chars };
   Punctuator PT(S);
-  Parser P(T, PT, DS);
+  Parser P { T, PT, DS };
   LanguageConfig Config;
   auto SF = P.parseSourceFile();
   SF->setParents();
-  Checker C(Config, DS);
-  C.check(SF);
+  Checker C { DS };
+  C.run(SF);
   return std::make_tuple(SF, C, DS);
 }
 
@@ -37,17 +37,17 @@ auto checkExpression(std::string Input) {
 TEST(CheckerTest, InfersIntFromIntegerLiteral) {
   auto [Expr, Checker, DS] = checkExpression("1");
   ASSERT_EQ(DS.countDiagnostics(), 0);
-  ASSERT_EQ(Checker.getType(Expr), Checker.getIntType());
+  ASSERT_EQ(*Checker.getTypeOfNode(Expr), *Checker.getIntType());
 }
 
 TEST(CheckerTest, TestIllegalTypingVariable) {
   auto [SF, C, DS] = checkSourceFile("let a: Int = \"foo\"");
   ASSERT_EQ(DS.countDiagnostics(), 1);
   auto D1 = DS.Diagnostics[0];
-  ASSERT_EQ(D1->getKind(), DiagnosticKind::UnificationError);
-  auto Diag = static_cast<UnificationErrorDiagnostic*>(D1);
+  ASSERT_EQ(D1->getKind(), DiagnosticKind::TypeMismatchError);
+  auto Diag = static_cast<TypeMismatchError*>(D1);
   // TODO these types have to be sorted first
-  ASSERT_EQ(Diag->getLeft(), C.getIntType());
-  ASSERT_EQ(Diag->getRight(), C.getStringType());
+  ASSERT_EQ(Diag->Left, C.getIntType());
+  ASSERT_EQ(Diag->Right, C.getStringType());
 }
 
