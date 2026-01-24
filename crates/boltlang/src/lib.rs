fa2cabd44066256fefb02f20e87eb28974c1a1a8
Sam Vervaeck <samvv@pm.me>
2026-01-24 14:35:09 +0100

    Document re-exports

--- crates/boltlang/src/lib.rs
+++ crates/boltlang/src/lib.rs
@@ -4,9 +4,13 @@ mod db;
 mod syntax;
 mod parser;
 
+/// Re-export of the Salsa library that boltlang uses
+pub use salsa;
+
+/// Re-export of the Rowan library that boltlang uses
+pub use rowan;
+
 pub use {
-    rowan,
-    salsa,
     syntax::{SyntaxKind, SyntaxNode, SyntaxToken, SyntaxElement},
     db::{BoltDatabaseImpl, parse, SourceProgram, line_column_of_offset, start_offset_of_line, end_offset_of_line, index_lines},
     parser::SyntaxError
